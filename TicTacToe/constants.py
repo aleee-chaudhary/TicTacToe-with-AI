ae6061470277759a7a3fcf96c58a3c70559f539d
Muhammad Ali <94305336+aleee-chaudhary@users.noreply.github.com>
2025-02-06 14:11:23 +0100

    constants used

---
+++ TicTacToe/constants.py
@@ -0,0 +1,25 @@
+# Window dimensions
+WIDTH = 600
+HEIGHT = 600
+
+# Board dimensions
+ROWS = 3
+COLS = 3
+SQSIZE = WIDTH // COLS  # Square size
+
+# Colors
+BG_COLOR = (28, 170, 156)
+LINE_COLOR = (23, 145, 135)
+CIRCLE_COLOR = (239, 231, 200)
+CROSS_COLOR = (66, 66, 66)
+
+# Circle parameters
+RADIUS = SQSIZE // 4
+CIRCLE_WIDTH = 15
+
+# Cross parameters
+CROSS_WIDTH = 20
+OFFSET = 50
+
+# Line width
+LINE_WIDTH = 15
